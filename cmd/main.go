4c7327c847aa3c0800b733e7320d2fd272362b01
Jason Jordan <jlj5aj@virginia.edu>
2020-02-03 15:57:07 -0500

    add version/healthcheck endpoint stubs

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"path/filepath"
 	"fmt"
 	"log"
+	"net/http"
+	"runtime"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
@@ -11,6 +15,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
+
+// git commit used for this build; supplied at compile time
+var gitCommit string
 
 /**
  * Main entry point for the web service
@@ -46,13 +53,12 @@ func main() {
 		h.ServeHTTP(c.Writer, c.Request)
 	})
 
+	router.GET("/favicon.ico", ignoreHandler)
+
+	router.GET("/version", versionHandler)
+	router.GET("/healthcheck", healthCheckHandler)
+
 /*
-	router.GET("/favicon.ico", svc.ignoreHandler)
-
-	router.GET("/version", svc.versionHandler)
-	router.GET("/identify", svc.identifyHandler)
-	router.GET("/healthcheck", svc.healthCheckHandler)
-
 	if api := router.Group("/api"); api != nil {
 		api.POST("/suggest", svc.authenticateHandler, svc.suggestHandler)
 	}
@@ -64,4 +70,57 @@ func main() {
 	log.Printf("Start service on %s", portStr)
 
 	log.Fatal(router.Run(portStr))
+}
+
+func ignoreHandler(c *gin.Context) {
+}
+
+func buildVersion() string {
+	files, _ := filepath.Glob("buildtag.*")
+	if len(files) == 1 {
+		return strings.Replace(files[0], "buildtag.", "", 1)
+	}
+
+	return "unknown"
+}
+
+func versionHandler(c *gin.Context) {
+	type vResp struct {
+		BuildVersion string `json:"build,omitempty"`
+		GoVersion    string `json:"go_version,omitempty"`
+		GitCommit    string `json:"git_commit,omitempty"`
+	}
+
+	ver := vResp {
+		BuildVersion: buildVersion(),
+		GoVersion:    fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH),
+		GitCommit:    gitCommit,
+    }
+
+	c.JSON(http.StatusOK, ver)
+}
+
+func healthCheckHandler(c *gin.Context) {
+	type hcResp struct {
+		Healthy bool   `json:"healthy"`
+		Message string `json:"message,omitempty"`
+	}
+
+	hcMap := make(map[string]hcResp)
+
+	hcSolr := hcResp{}
+
+	status := http.StatusOK
+	hcSolr = hcResp{Healthy: true}
+
+/*
+	if err := s.handlePingRequest(); err != nil {
+		status = http.StatusInternalServerError
+		hcSolr = hcResp{Healthy: false, Message: err.Error()}
+	}
+*/
+
+	hcMap["solr"] = hcSolr
+
+	c.JSON(status, hcMap)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,17 @@ module github.com/uvalib/virgo4-suggestor-ws
 go 1.13
 
 require (
-	github.com/gin-contrib/cors v1.3.0 // indirect
-	github.com/gin-contrib/gzip v0.0.1 // indirect
-	github.com/gin-gonic/gin v1.5.0 // indirect
-	github.com/prometheus/client_golang v1.4.0 // indirect
-	github.com/zsais/go-gin-prometheus v0.1.0 // indirect
+	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-contrib/gzip v0.0.1
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/prometheus/client_golang v1.4.0
+	github.com/zsais/go-gin-prometheus v0.1.0
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
